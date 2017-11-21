c250419f2cf083bf5f50fe0a4b9839036e874dbf
mcraigie <max.craigie@gmail.com>
2017-11-21 18:02:36 +1100

    add basic errors and minimum size check

---
+++ errors.rb
@@ -0,0 +1,17 @@
+class CellDataSizeError < StandardError
+  def initialize(msg="Cell data does not conform to specified size")
+    super(msg)
+  end
+end
+
+class BelowMinimumSizeError < StandardError
+  def initialize(msg="Size must be >= 3")
+    super(msg)
+  end
+end
+
+class CellDataNilError < StandardError
+  def initialize(msg="Cell data contains nil")
+    super(msg)
+  end
+end
