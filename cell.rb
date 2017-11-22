7aed96cc1cee9eac3b5a7ba20f4a2ece7d71fe9b
mcraigie <max.craigie@gmail.com>
2017-11-22 12:23:11 +1100

    implement most of Grid#match?

--- cell.rb
+++ cell.rb
@@ -1,9 +1,17 @@
 module Bongard
   class Cell
     attr_reader :value
+    attr_accessor :up
+    attr_accessor :down
+    attr_accessor :left
+    attr_accessor :right
 
     def initialize(value)
       @value = value
+    end
+
+    def match(regex)
+      value.to_s.match(regex)
     end
   end
 end
--- grid.rb
+++ grid.rb
@@ -3,16 +3,16 @@ require './cell.rb'
 
 module Bongard
 
-# cell_data comes in like this:
-# [ [1,2,3],
-#   [4,5,6],
-#   [7,8,9] ]
+  # cell_data comes in like this:
+  # [ [1,2,3],
+  #   [4,5,6],
+  #   [7,8,9] ]
 
-# cells at 1-indexed and the origin is top left
-# e.g.
-# (1,1) (2,1) (3,1)
-# (1,2) (2,2) (3,2)
-# (1,3) (2,3) (3,3)
+  # cells at 1-indexed and the origin is top left
+  # e.g.
+  # (1,1) (2,1) (3,1)
+  # (1,2) (2,2) (3,2)
+  # (1,3) (2,3) (3,3)
 
   class Grid
     attr_reader :size
@@ -33,6 +33,8 @@ module Bongard
       @cells = @rows.flatten
       @edge_cells = calculate_edge_cells
       @corner_cells = calculate_corner_cells
+
+      prime_cell_neighbours
     end
 
     def conforms_to_size?(cell_data)
@@ -44,6 +46,18 @@ module Bongard
     def contains_nil?(cell_data)
       return true if cell_data.any? { |row| row.nil? || row.any? { |e| e.nil? } }
       return false
+    end
+
+    def prime_cell_neighbours
+      (1..size).each do |col_id|
+        (1..size).each do |row_id|
+          cell = cell_at(col_id, row_id)
+          cell.up = cell_at(col_id, row_id - 1)
+          cell.down = cell_at(col_id, row_id + 1)
+          cell.left = cell_at(col_id - 1, row_id)
+          cell.right = cell_at(col_id + 1, row_id)
+        end
+      end
     end
 
     def each(&block)
@@ -106,8 +120,94 @@ module Bongard
       cell_at(half_way, half_way)
     end
 
-    def match?(pattern_selector)
-      
+    def match?(pattern)
+      steps = convert_pattern(pattern)
+
+      # currently check every cell
+      # could make more efficient by analysing the pattern and ruling out
+      # section of the grid that couldn't possibly start a match
+      self.each do |starting_cell|
+        current_cell = starting_cell
+        pattern_found = true
+
+        steps.each do |step|
+          unless current_cell.match(step[:test])
+            pattern_found = false
+            break
+          end
+
+          current_cell = relative_cell(current_cell, step)
+        end
+
+        # exit as soon as possible if the pattern is found
+        return true if pattern_found
+      end
+
+      return false
+    end
+
+    def relative_cell(starting_cell, delta)
+      current_cell = starting_cell
+      row_diff = delta[:left] - delta[:right]
+      col_diff = delta[:up] - delta[:down]
+
+      current_cell = walk_row(starting_cell, row_diff)
+      current_cell = walk_col(starting_cell, col_diff)
+
+      return current_cell
+    end
+
+    def walk_row(starting_cell, row_diff)
+      current_cell = starting_cell
+      if row_diff > 0
+        direction = :left
+      elsif row_diff < 0
+        row_diff * -1
+        direction = :right
+      end
+
+      row_diff.times do
+        current_cell = current_cell.__send__(direction)
+      end
+
+      return current_cell
+    end
+
+    def walk_col(starting_cell, col_diff)
+      current_cell = starting_cell
+      if col_diff > 0
+        direction = :up
+      elsif col_diff < 0
+        col_diff * -1
+        direction = :down
+      end
+
+      col_diff.times do
+        current_cell = current_cell.__send__(direction)
+      end
+
+      return current_cell
+    end
+
+    def convert_pattern(pattern)
+      #raise error if the pattern is crap
+      pattern.split('>')
+      [
+        {
+          test: /[1]/,
+          left: 0,
+          right: 1,
+          up: 0,
+          down: 1
+        },
+        {
+          test: /[6]/,
+          left: 0,
+          right: 0,
+          up: 0,
+          down: 0
+        }
+      ]
     end
 
     def rotate(n); end
